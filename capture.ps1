16f5a7d16ca424c9bdbd32f04faa06599d63a8b2
NK <nkhtnkht@gmail.com>
2026-05-19 21:29:36 +0900

    chore: 一時スクショ補助スクリプトを削除

--- capture.ps1
+++
@@ -1,9 +0,0 @@
-param([string]$out)
-Add-Type -AssemblyName System.Windows.Forms,System.Drawing
-$screen = [System.Windows.Forms.Screen]::PrimaryScreen.Bounds
-$bmp = New-Object System.Drawing.Bitmap $screen.Width, $screen.Height
-$g = [System.Drawing.Graphics]::FromImage($bmp)
-$g.CopyFromScreen(0, 0, 0, 0, $bmp.Size)
-$bmp.Save($out)
-$g.Dispose(); $bmp.Dispose()
-Write-Output "Saved: $out"
